5f6a3411b745ccea4dd165bc50bf5f05f2edc4be
thomasho1613 <thomasho1613@gmail.com>
2026-04-28 11:24:16 +0800

    Major update on the prediction model

--- sw.js
+++ sw.js
@@ -1,31 +1,22 @@
-var CACHE = 'brew-coach-v043';
-var FILES = ['./', './index.html', './manifest.json'];
+const CACHE = 'brew-coach-v045';
+const FILES = ['./', './index.html', './manifest.json'];
 
-self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open(CACHE).then(function(cache) {
-      return cache.addAll(FILES);
-    })
-  );
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
   self.skipWaiting();
 });
 
-self.addEventListener('activate', function(e) {
+self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys().then(function(names) {
-      return Promise.all(
-        names.filter(function(n) { return n !== CACHE; })
-             .map(function(n) { return caches.delete(n); })
-      );
-    })
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
   );
   self.clients.claim();
 });
 
-self.addEventListener('fetch', function(e) {
+self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(function(r) {
-      return r || fetch(e.request);
-    })
+    caches.match(e.request).then(r => r || fetch(e.request))
   );
 });
